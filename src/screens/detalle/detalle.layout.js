32b3188c97fb0a2279e6b568bd2c31e1a35bb56a
Diego Plaza <diegoplaza@MacBook-Air-de-Diego.local>
2021-02-25 16:47:04 -0300

    feat: add chart

--- src/screens/detalle/detalle.layout.js
+++ src/screens/detalle/detalle.layout.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import {View} from 'react-native';
-import {Text, ListItem, Input} from 'react-native-elements';
+import {Text, ListItem} from 'react-native-elements';
 import Moment from 'moment';
 import {StyleSheet} from 'react-native';
+import {LineChart, YAxis, XAxis, Grid} from 'react-native-svg-charts';
 
 const IndicadorLayout = (props) => {
-  const {detalles, unidadMedida, valorActual} = props;
+  const {
+    detalles,
+    unidadMedida,
+    valorActual,
+    valoresGraficosEjeY,
+    valoresGraficosEjeX,
+  } = props;
+  const contentInset = {top: 20, bottom: 20};
+  const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80];
   return (
     <View>
       <ListItem bottomDivider>
@@ -30,6 +39,30 @@ const IndicadorLayout = (props) => {
           </View>
         </ListItem.Content>
       </ListItem>
+      <View style={{height: 200, flexDirection: 'row'}}>
+        <YAxis
+          data={valoresGraficosEjeY}
+          style={{marginBottom: 10}}
+          contentInset={{top: 10, bottom: 10}}
+          svg={{fontSize: 10, fill: 'grey'}}
+        />
+        <View style={{flex: 1, marginLeft: 10}}>
+          <LineChart
+            style={{flex: 1}}
+            data={valoresGraficosEjeY}
+            contentInset={{top: 10, bottom: 10}}
+            svg={{stroke: 'rgb(134, 65, 244)'}}>
+            <Grid />
+          </LineChart>
+          <XAxis
+            style={{marginHorizontal: 10, height: 10}}
+            data={valoresGraficosEjeX}
+            formatLabel={(value, index) => index}
+            contentInset={{left: 10, right: 10}}
+            svg={{axefontSize: 10, fill: 'grey'}}
+          />
+        </View>
+      </View>
     </View>
   );
 };
--- src/screens/detalle/detalle.screen.js
+++ src/screens/detalle/detalle.screen.js
@@ -2,10 +2,14 @@ import React, {useEffect, useState} from 'react';
 import DetalleLayout from './detalle.layout';
 import indicadoresServices from '../../services/indicadoresServices';
 import {connect} from 'react-redux';
+import Moment from 'moment';
 
 const DetalleScreen = ({navigation, codigo, nombre, unidadMedida}) => {
   const [detalles, setDetalles] = useState({});
   const [valorActual, setValorActual] = useState({});
+  const [valoresGraficosEjeY, setValoresGraficosEjeY] = useState([]);
+  const [valoresGraficosEjeX, setValoresGraficosEjeX] = useState([]);
+
 
   useEffect(() => {
     (async () => {
@@ -22,12 +26,21 @@ const DetalleScreen = ({navigation, codigo, nombre, unidadMedida}) => {
     const response = await indicadoresServices.getIndicador(indicador);
     setDetalles(response.data);
     setValorActual(response.data.serie[0]);
+    const tempData = response.data.serie.slice(0, 10);
+    let resultY = tempData.map((a) => a.valor);
+    let resultX = tempData.map((a) => Moment(a.fecha).format('YYYY-MM-DD'));
+    console.log(resultX);
+    setValoresGraficosEjeY(resultY);
+    setValoresGraficosEjeX(resultX);
   };
+
   return (
     <DetalleLayout
       detalles={detalles}
       valorActual={valorActual}
       unidadMedida={unidadMedida}
+      valoresGraficosEjeY={valoresGraficosEjeY}
+      valoresGraficosEjeX={valoresGraficosEjeX}
     />
   );
 };
